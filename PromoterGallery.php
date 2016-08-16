489d69f3b22477b04bf96354008674c839f4bf48
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2016-08-16 18:06:40 +0300

    Update icons markup for Font-Awesome 4.6

--- PromoterGallery.php
+++ PromoterGallery.php
@@ -40,8 +40,8 @@ class PromoterGallery {
 	        . '<h5 class="sr-only">זוהי גלריה המקדמת ערכים שונים באתר.</h5>'
 			. '<div class="gallery-controls">'
 		        . '<span class="sr-only">בכל רגע מוצגות 3 ידיעות בגלריה. ניתן להציג ידיעה נוספת או לחזור לאחור באמצעות הכפתורים הבאים, או באמצעות מקשי החיצים כאשר הפוקוס הוא על הגלריה</span>'
-				. '<a href="#" class="owl-prev"><span class="icon icon-chevron-right icon-large" title="הקודם"></span><span class="sr-only">הצגת הידיעה הקודמת</span></a>'
-				. '<a href="#" class="owl-next"><span class="icon icon-chevron-left icon-large" title="הבא"></span><span class="sr-only">הצגת הידיעה הבאה</span></a>'
+				. '<a href="#" class="owl-prev"><span class="fa fa-chevron-right fa-lg" title="הקודם"></span><span class="sr-only">הצגת הידיעה הקודמת</span></a>'
+				. '<a href="#" class="owl-next"><span class="fa fa-chevron-left fa-lg" title="הבא"></span><span class="sr-only">הצגת הידיעה הבאה</span></a>'
 			. '</div>'
 		;
 		if( $args['title'] ) {
