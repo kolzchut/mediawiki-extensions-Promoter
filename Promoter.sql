a74cde644187da5dcd6b3d3bb5ed760c32a74db1
Ardethian <ardethian@gmail.com>
2018-04-30 17:13:15 +0300

    updated db tables to support ad dates

--- Promoter.sql
+++ Promoter.sql
@@ -3,31 +3,32 @@
 -- See documentation at [...]
 
 CREATE TABLE IF NOT EXISTS /*_*/pr_campaigns (
-  cmp_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  cmp_name varchar(255) NOT NULL,
-  cmp_enabled tinyint(1) NOT NULL DEFAULT '0',
-  cmp_archived tinyint(1) NOT NULL DEFAULT '0'
+    cmp_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cmp_name varchar(255) NOT NULL,
+    cmp_enabled tinyint(1) NOT NULL DEFAULT '0',
+    cmp_archived tinyint(1) NOT NULL DEFAULT '0'
 ) /*$wgDBTableOptions*/;
 
 
 CREATE TABLE IF NOT EXISTS /*_*/pr_ads (
 	ad_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	ad_name varchar(255),
-  ad_title varchar(255) NOT NULL,
-  ad_text text NOT NULL,
-  ad_mainlink varchar(255),
-  ad_display_anon tinyint(1) NOT NULL DEFAULT '1',
+    ad_title varchar(255) NOT NULL,
+    ad_text text NOT NULL,
+    ad_mainlink varchar(255),
+    ad_display_anon tinyint(1) NOT NULL DEFAULT '1',
 	ad_display_user tinyint(1) NOT NULL DEFAULT '1',
-	ad_tag_new tinyint(1) NOT NULL DEFAULT '0'
+	ad_tag_new tinyint(1) NOT NULL DEFAULT '0',
+	ad_date_start BINARY(14) DEFAULT NULL,
+    ad_date_end BINARY(14) DEFAULT NULL
 ) /*$wgDBTableOptions*/;
-
 
 -- Cross-reference table between campaigns and ads
 CREATE TABLE IF NOT EXISTS /*_*/pr_adlinks (
 	adl_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   -- Ad to attach to a campaign... (Foreign Key to pr_ads).
-  ad_id int(11) NOT NULL,
+    ad_id int(11) NOT NULL,
   -- AdCampaign the ad will belong to (Foreign Key to pr_campaigns).
-  cmp_id int(11) NOT NULL,
+    cmp_id int(11) NOT NULL,
 	adl_weight int(11) NOT NULL
 ) /*$wgDBTableOptions*/;
